fcc494d24a153cdfe1414aa7a52d7b9db21a6c12
CHAMILKA MIHIRAJ <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-08-31 23:42:21 +0530

    Update publications.ts

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -10,16 +10,30 @@ export type Publication = {
 export const publications: Publication[] = [
   {
     id: "pub-2024-vision-agri",
-    title: "Research on Computer Vision in Smart Agriculture",
-    link: "#",
-    year: "2024",
-    venue: "International Journal of AgriTech",
+    title: "All About CI/CD Pipelines - Making Software Delivery Smooth and Fast",
+    link: "https://medium.com/@chamilkaperera5/all-about-ci-cd-pipelines-making-software-delivery-smooth-and-fast-505c99372f84",
+    year: "2025",
+    venue: "Medium articles",
   },
   {
     id: "pub-2025-meal-ml",
-    title: "Optimizing Meal Token Allocation with ML",
-    link: "#",
-    year: "2025",
-    venue: "ACM Student Research Conference",
+    title: "Numpy - python open source library",
+    link: "https://medium.com/@chamilkaperera5/numpy-short-for-numerical-python-is-a-widely-used-open-source-library-in-the-python-programming-547d8e9169b8",
+    year: "2024",
+    venue: "Medium articles",
+  },
+  {
+    id: "pub-2025-meal-ml",
+    title: "Unleashing GPU Power",
+    link: "https://medium.com/@chamilkaperera5/unleashing-gpu-power-navigating-onboard-vs-a651b67e91af",
+    year: "2023",
+    venue: "Medium articles",
+  },
+  {
+    id: "pub-2025-meal-ml",
+    title: "Exploring ALU, CU, Register Memory, Cache, and CPU Speed",
+    link: "https://medium.com/@chamilkaperera5/numpy-short-for-numerical-python-is-a-widely-used-open-source-library-in-the-python-programming-547d8e9169b8",
+    year: "2023",
+    venue: "Medium articles",
   },
 ];
